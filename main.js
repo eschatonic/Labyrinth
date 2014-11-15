ded1f959483cb738868a659322add56907947dd4
David Stark <dhmholley@gmail.com>
2014-11-15 00:02:04 +0000

    Substantial updates

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 /* variables */
 var lab = {
 	seed:Math.random(),
-	graph:[[]],
+	graph:{
+		"-1":[],
+		"0":[],
+		"1":[]
+	},
 	player:{
 		location:{},
-		visibility:11,
-		litVisibility:7
-	}
+		visibility:9,
+		litVisibility:7,
+		treasure:0
+	},
+	clicked:0
 };
 
 var data = {
@@ -25,15 +31,14 @@ function setup(){
 	randomSeed(lab.seed);
 	noiseSeed(lab.seed);
 	
-	for (var i=-1; i<=1; i++){
-		lab.graph[i] = [];
-		for (var j=-1; j<=1; j++){
-			lab.graph[i][j] = new Node({y:i,x:j},data.nodes.nodeTypes.open,[])
-		}
-	}
 	lab.player.location = {
 		y:0,
 		x:0
+	}
+	for (var y=-1;y<=1;y++){
+		for (var x=-1;x<=1;x++){
+			generateNode(y,x,"open");
+		}
 	}
 	explore();
 	
@@ -48,9 +53,117 @@ function initialiseData(){
 		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_65.png"),
 		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_179.png")
 	);
+	data.nodes.floorTypes.stone0 = new FloorType(
+		"Stone",
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1443.png"),
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1440.png")
+	);
+	data.nodes.floorTypes.stone1 = new FloorType(
+		"Stone",
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1384.png"),
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1381.png")
+	);
+	data.nodes.floorTypes.stone2 = new FloorType(
+		"Stone",
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1385.png"),
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1382.png")
+	);
+	data.nodes.floorTypes.stone3 = new FloorType(
+		"Stone",
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1386.png"),
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1383.png")
+	);
+	data.nodes.floorTypes.stone4 = new FloorType(
+		"Stone",
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1442.png"),
+		loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_1439.png")
+	);
+	
+	data.nodes.wallImages = [[
+		/*[ //bright grey
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_125.png"), //single
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_126.png"), //right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_127.png"), //left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_128.png"), //left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_129.png"), //down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_130.png"), //up-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_131.png"), //up
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_132.png"), //right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_133.png"), //left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_134.png"), //up-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_135.png"), //up-left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_136.png"), //up-down-left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_137.png"), //left-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_138.png"), //up-left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_139.png"), //up-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_140.png")  //up-left-right
+		],*/
+		[ //dull yellow
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_695.png"), //single
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_696.png"), //right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_697.png"), //left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_698.png"), //left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_699.png"), //down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_700.png"), //up-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_701.png"), //up
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_702.png"), //right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_703.png"), //left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_704.png"), //up-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_705.png"), //up-left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_706.png"), //up-down-left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_707.png"), //left-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_708.png"), //up-left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_709.png"), //up-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_710.png")  //up-left-right
+		],
+		[ //bright grey
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_125.png"), //single
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_126.png"), //right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_127.png"), //left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_128.png"), //left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_129.png"), //down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_130.png"), //up-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_131.png"), //up
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_132.png"), //right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_133.png"), //left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_134.png"), //up-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_135.png"), //up-left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_136.png"), //up-down-left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_137.png"), //left-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_138.png"), //up-left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_139.png"), //up-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_140.png")  //up-left-right
+		]
+		/*[ //Bright yellow
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_182.png"), //single
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_183.png"), //right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_184.png"), //left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_185.png"), //left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_186.png"), //down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_187.png"), //up-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_188.png"), //up
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_189.png"), //right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_190.png"), //left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_191.png"), //up-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_192.png"), //up-left
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_193.png"), //up-down-left-right
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_194.png"), //left-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_195.png"), //up-left-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_196.png"), //up-right-down
+			loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_197.png")  //up-left-right
+		]*/
+	]]
 
-	data.nodes.nodeTypes.open = new NodeType(true,data.nodes.floorTypes.earth)
-	data.nodes.nodeTypes.wall = new NodeType(false,null);
+	data.nodes.treasure = loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_261.png");
+	data.nodes.treasureOpened = loadImage("spritePack/Sliced/world_24x24/oryx_16bit_fantasy_world_264.png");
+	
+	data.nodes.nodeTypes.open = new NodeType(true,data.nodes.floorTypes.earth,false);
+	data.nodes.nodeTypes.stone0 = new NodeType(true,data.nodes.floorTypes.stone0,false);
+	data.nodes.nodeTypes.stone1 = new NodeType(true,data.nodes.floorTypes.stone1,false);
+	data.nodes.nodeTypes.stone2 = new NodeType(true,data.nodes.floorTypes.stone2,false);
+	data.nodes.nodeTypes.stone3 = new NodeType(true,data.nodes.floorTypes.stone3,false);
+	data.nodes.nodeTypes.stone4 = new NodeType(true,data.nodes.floorTypes.stone4,false);
+	data.nodes.nodeTypes.wall = new NodeType(false,false,0);
 }
 
 /* constructors */
@@ -59,9 +172,10 @@ function Node(location,nodeType,contains){
 	this.nodeType = nodeType;
 	this.contains = contains;
 }
-function NodeType(traversable,floor){
+function NodeType(traversable,floor,wall){
 	this.traversable = traversable;
-	if (traversable) this.floor = floor;
+	this.floor = floor;
+	this.wall = wall;
 }
 function FloorType(name,img,imgVisible){
 	this.name = name;
@@ -78,6 +192,7 @@ function draw(){
 	drawGraph();
 	drawVisible();
 	drawPlayer();
+	drawScore();
 }
 function drawGraph(){
 	for (var row in lab.graph){
@@ -92,6 +207,8 @@ function drawNode(node){
 		
 	if (node.nodeType.floor){
 		image(node.nodeType.floor.img,windowWidth/2 + offsetX*data.nodes.size,windowHeight/2 + offsetY*data.nodes.size);
+	} else if (node.nodeType.wall !== false){
+		image(getWallImg(node.location.y,node.location.x,0),windowWidth/2 + offsetX*data.nodes.size,windowHeight/2 + offsetY*data.nodes.size);
 	}
 }
 function drawVisible(){
@@ -101,8 +218,18 @@ function drawVisible(){
 				var node = lab.graph[i][j];
 				var offsetY = node.location.y - lab.player.location.y,
 					offsetX = node.location.x - lab.player.location.x;
+					
 				if (node.nodeType.floor){
 					image(node.nodeType.floor.imgVisible,windowWidth/2 + offsetX*data.nodes.size,windowHeight/2 + offsetY*data.nodes.size);
+				} else if (node.nodeType.wall !== false){
+					image(getWallImg(node.location.y,node.location.x,1),windowWidth/2 + offsetX*data.nodes.size,windowHeight/2 + offsetY*data.nodes.size);
+				}
+				
+				if (node.contains && node.contains.length > 0){
+					for (var thing in node.contains){
+						if (node.contains[thing] == "treasure") image(data.nodes.treasure,windowWidth/2 + offsetX*data.nodes.size,windowHeight/2 + offsetY*data.nodes.size)
+						if (node.contains[thing] == "treasureOpened") image(data.nodes.treasureOpened,windowWidth/2 + offsetX*data.nodes.size,windowHeight/2 + offsetY*data.nodes.size)
+					}
 				}
 			}
 		}
@@ -110,6 +237,63 @@ function drawVisible(){
 }
 function drawPlayer(){
 	image(lab.player.img,windowWidth/2,windowHeight/2);
+}
+function drawScore(){
+	stroke(255);
+	fill(255);
+	textSize(18);
+	text("Treasure: " + lab.player.treasure,20,38)
+}
+
+function getWallImg(y,x,set){
+	var node = lab.graph[y][x];
+	var wallType = 0;
+	if (isWall(y-1,x)){ //up
+		if (isWall(y,x+1)){ //right
+			if (isWall(y+1,x)){ //down
+				if (isWall(y,x-1)){ //left
+					wallType = 11; //=up-right-down-left
+				} else {
+					wallType = 14; //=up-right-down
+				}
+			} else if (isWall(y,x-1)){
+				wallType = 15; //up-right-left
+			} else {
+				wallType = 9; //up-right
+			}
+		} else if (isWall(y+1,x)){ //down
+			if (isWall(y,x-1)){ //left
+				wallType = 13; //=up-down-left
+			} else {
+				wallType = 5; //=up-down
+			}
+		} else if (isWall(y,x-1)){ //left
+			wallType = 10; //=up-left
+		} else {
+			wallType = 6; //=up
+		}
+	} else if (isWall(y,x+1)){ //right
+		if (isWall(y+1,x)){ //down
+			if (isWall(y,x-1)){ //left
+				wallType = 12; //=right-down-left
+			} else {
+				wallType = 7; //=right-down
+			}
+		} else if (isWall(y,x-1)){ //left
+			wallType = 2; //=right-left
+		} else {
+			wallType = 1; //=right
+		}
+	} else if (isWall(y+1,x)){ //down
+		if (isWall(y,x-1)){ //left
+			wallType = 8; //=down-left
+		} else {
+			wallType = 4; //=down
+		}
+	} else if (isWall(y,x-1)){ //left
+		wallType = 3; //=left
+	}
+	return data.nodes.wallImages[node.nodeType.wall][set][wallType];
 }
 
 function keyPressed() {
@@ -124,30 +308,35 @@ function keyPressed() {
     }
 	return false;
 }
-function mousePressed(){
-	if (mouseY < windowHeight/2){
-		if (mouseX < windowWidth/2){
-			if (mouseX/windowWidth < mouseY/windowHeight){
+function mouseClicked(){
+	var w = windowWidth;
+	var h = windowHeight;
+	var y = mouseY;
+	var x = mouseX;
+
+	if (y < h/2){
+		if (x < w/2){
+			if (x/w < y/h){
 				move(0,-1);
 			} else {
 				move(-1,0);
 			}
 		} else {
-			if ((windowWidth-mouseX)/windowWidth < mouseY/windowHeight){
+			if ((w-x)/w < y/h){
 				move(0,1);
 			} else {
 				move(-1,0);
 			}
 		}
 	} else {
-		if (mouseX < windowWidth/2){
-			if (mouseX/windowWidth < (windowHeight-mouseY)/windowHeight){
+		if (x < w/2){
+			if (x/w < (h-y)/h){
 				move(0,-1);
 			} else {
 				move(1,0);
 			}
 		} else {
-			if ((windowWidth-mouseX)/windowWidth < (windowHeight-mouseY)/windowHeight){
+			if ((w-x)/w < (h-y)/h){
 				move(0,1);
 			} else {
 				move(1,0);
@@ -155,10 +344,24 @@ function mousePressed(){
 		}
 	}
 }
+
 function move(dy,dx){
-	lab.player.location.y += dy;
-	lab.player.location.x += dx;
-	explore();
+	if (!isWall(lab.player.location.y + dy,lab.player.location.x + dx)){
+		lab.player.location.y += dy;
+		lab.player.location.x += dx;
+		explore();
+		
+		var c = lab.graph[lab.player.location.y][lab.player.location.x].contains;
+		if (c.indexOf("treasure") > -1){
+			c[c.indexOf("treasure")] = "treasureOpened";
+			lab.player.treasure += 100;
+		}
+	}
+}
+function isWall(y,x){
+	if (typeof lab.graph[y] == "undefined" || typeof lab.graph[y][x] == "undefined" || typeof lab.graph[y][x].nodeType == "undefined") return false;
+	if (lab.graph[y][x].nodeType.wall === false) return false;
+	return true;
 }
 function explore(){
 	for (var i=(lab.player.visibility * -1) + lab.player.location.y; i<=lab.player.visibility + lab.player.location.y; i++){
@@ -166,12 +369,46 @@ function explore(){
 		for (var j=(lab.player.visibility * -1) + lab.player.location.x; j<=lab.player.visibility + lab.player.location.x; j++){
 			if (Math.floor(Math.sqrt(Math.pow(i - lab.player.location.y,2) + Math.pow(j - lab.player.location.x,2))) < lab.player.visibility){
 				if (typeof lab.graph[i][j] == "undefined"){
-					generateNode(i,j);
+					generateNode(i,j,false);
 				}
 			}
 		}
 	}
 }
-function generateNode(y,x){
-	lab.graph[y][x] = new Node({y:y,x:x},data.nodes.nodeTypes.open,[])
+
+function generateNode(y,x,force){
+	lab.graph[y][x] = new Node({y:y,x:x},data.nodes.nodeTypes[getNodeType(y,x,force)],[]);
+	lab.graph[y][x].contains = generateContents(y,x);
+}
+function getNodeType(y,x,force){
+	if (force == "open") return getOpenSpace();
+	if (force == "wall") return getWall();
+	
+	if (noise(y, x) < 0.3) return getWall();
+	return getOpenSpace();
+}
+function getOpenSpace(){
+	var arr = [
+		"stone0","stone0","stone0","stone0",
+		"stone0","stone0","stone0","stone0",
+		"stone0","stone0","stone0","stone0",
+		"stone1","stone2","stone3","stone4"
+	];
+	var index = Math.floor(random() * arr.length);
+	return arr[index];
+}
+function getWall(){
+	return "wall";
+}
+function generateContents(y,x){
+	if (isWall(y,x)){
+		return [];
+	} else {
+		var contents = [];
+		
+		var r = random();
+		if (r < 0.005) contents.push("treasure");
+		
+		return contents;
+	}
 }
